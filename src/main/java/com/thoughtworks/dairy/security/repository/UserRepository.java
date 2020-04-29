b7b26e35de3c943311b96844ff0c32ba4bf02f2a
Ayush Saraf <ayush.saraf47@gmail.com>
2020-04-29 14:15:05 +0530

    Added : model and repository for registered users

---
+++ src/main/java/com/thoughtworks/dairy/security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.dairy.security.repository;
+
+import com.thoughtworks.dairy.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUserName(String userName);
+}
