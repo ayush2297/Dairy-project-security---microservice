902a1dc26853ca9da60f74b599b07e199218c506
Ayush Saraf <ayush.saraf47@gmail.com>
2020-04-30 02:33:11 +0530

    Added : custom filter to the config

--- src/main/java/com/thoughtworks/dairy/security/configuration/SecurityConfig.java
+++ src/main/java/com/thoughtworks/dairy/security/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.dairy.security.configuration;
 
-import com.thoughtworks.dairy.security.service.CustomerDetailsService;
+import com.thoughtworks.dairy.security.filter.JwtFilter;
+import com.thoughtworks.dairy.security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,17 +11,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private CustomerDetailsService customerDetailsService;
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customerDetailsService);
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Override
@@ -38,6 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated()
-                .and().httpBasic();
+                .and().httpBasic()
+                .and().exceptionHandling()
+                .and().sessionManagement().sessionCreationPolicy(STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
