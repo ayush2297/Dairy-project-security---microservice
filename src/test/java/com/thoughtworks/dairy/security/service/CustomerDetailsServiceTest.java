3ff586e3bf62739378c9b9f5cd0a88e7ecf47e1b
Ayush Saraf <ayush.saraf47@gmail.com>
2020-04-29 15:09:21 +0530

    Added : test for customerdetailsservice

---
+++ src/test/java/com/thoughtworks/dairy/security/service/CustomerDetailsServiceTest.java
@@ -0,0 +1,62 @@
+package com.thoughtworks.dairy.security.service;
+
+import com.thoughtworks.dairy.security.exception.AuthenticationServerException;
+import com.thoughtworks.dairy.security.model.User;
+import com.thoughtworks.dairy.security.repository.UserRepository;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+public class CustomerDetailsServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private CustomerDetailsService customerDetailsService;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @org.junit.Test
+    public void shouldLoadUserByUsername() {
+        Long id = 1L;
+        String userName = "userName";
+        String pass = "pass";
+        String email = "email@gmail.com";
+        User user = new User(id, userName, pass, email);
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(user));
+
+        UserDetails userDetails = customerDetailsService.loadUserByUsername(userName);
+        assertEquals(userName,userDetails.getUsername());
+        assertEquals(pass,userDetails.getPassword());
+        verify(userRepository,times(1)).findByUserName(userName);
+    }
+
+    @org.junit.Test
+    public void shouldThrowExceptionIfUserByUsernameDoesNotExist() {
+        String userName = "userName";
+        expectedException.expect(AuthenticationServerException.class);
+        expectedException.expectMessage("user not found!");
+        when(userRepository.findByUserName(userName)).thenThrow(new AuthenticationServerException("user not found!", BAD_REQUEST));
+
+        UserDetails userDetails = customerDetailsService.loadUserByUsername(userName);
+    }
+
+
+
+}
